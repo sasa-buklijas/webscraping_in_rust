9e513a5b54d6fe5e1bc030abf13702cc32012221
Sasa Buklijas <buklijas.sasa@gmail.com>
2022-11-28 18:37:28 +0100

    Update main.rs
    implemented as structure

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use reqwest::Url;
 use scraper::{Html, Selector};
 
-struct Price {
+// TODO, maybe add price as separate structure
+/*struct Price {
     old: f32,
     discounted: f32,
-}
-struct Item {
-    category: String,   // mislim da bi ova tri trebalo staviti kao
-    name: String,
-    url: String,
-    discount: i8,
-    price: Price,
+}*/
+
+#[derive(Debug)]
+struct Item<'a> {   // <'a> is lifetime
+    category: &'a str,   
+    name: &'a str,
+    url: &'a str,
+    discount: u8,
+    //discount: &'a str,
+    price_old: f32,
+    price_discounted: f32,
+    //price_old: &'a str,
+    //price_discounted: &'a str,
     available: bool,
 }
 
@@ -43,20 +50,40 @@ fn main() {
     for element in document.select(&item_selector) {
         let item_category_element = element.select(&item_category_selector).next().expect("Could not select item category.");
         let item_category = item_category_element.text().collect::<String>();
+        let item_category = item_category.as_str();
 
         let item_name_element = element.select(&item_name_selector).next().expect("Could not select item name.");
         let item_name = item_name_element.text().collect::<String>();
+        let item_name = item_name.as_str();
         let item_url = item_name_element.value().attr("href").expect("Could not find item href attribute.");
     
 
         let item_discount_element =  element.select(&item_discount_selector).next().expect("Could not select item discount.");
-        let item_discount = item_discount_element.text().collect::<&>();
+        let mut item_discount = item_discount_element.text().collect::<String>();
+        item_discount.pop();            // remove last char
+        item_discount.remove(0);    // remove first char    // https://stackoverflow.com/a/70598494/2006674
+        let item_discount = item_discount.parse::<u8>().unwrap();
+        //println!("      {:?}", item_discount); panic!();
 
         let item_old_price_element =  element.select(&item_old_price_selector).next().expect("Could not select item old price.");
-        let item_old_price = item_old_price_element.text().collect::<String>();
-        
+        let mut item_old_price = item_old_price_element.text().collect::<String>();
+        item_old_price.pop(); 
+        item_old_price.pop(); // to remove space 
+        //let item_old_price = item_old_price.trim();
+        item_old_price = item_old_price.replace(",", ".");
+        let item_old_price = item_old_price.parse::<f32>().unwrap();
+        //println!("      {:?}", item_old_price); panic!();
+
         let item_discount_price_element =  element.select(&item_discount_price_selector).next().expect("Could not select item discount price.");
-        let item_discount_price = item_discount_price_element.text().collect::<&str>().trim();
+        let item_discount_price = item_discount_price_element.text().collect::<String>();
+        //println!("      {:?}", item_discount_price);    // "\n\t\t\t\t\t\t\t1,99 €\t\t\t\t\t\t\t\t\t\t\t\t\t"
+        let item_discount_price = item_discount_price.trim();
+        let item_discount_price = &item_discount_price[0..item_discount_price.len() - 4]; // because it take 2x per char // https://stackoverflow.com/a/65976485/2006674
+        //println!("      {:?}", item_discount_price); panic!();
+        //item_discount_price.pop(); 
+        //item_discount_price.pop(); // to remove space
+        let item_discount_price = item_discount_price.replace(",", ".");
+        let item_discount_price = item_discount_price.parse::<f32>().unwrap();
 
         let item_unavailable_element = element.select(&item_unavailable_selector).next();        
         // let item_available = if item_unavailable_element == None {true} else {false};
@@ -71,13 +98,13 @@ fn main() {
             item_available = false;
         }*/
 
-        println!("{:?}",item_name);
-        println!("      {:?}", item_category);
+        //println!("{:?}",item_name);
+        //println!("      {:?}", item_category);
         //println!("      {:?}", item_url);
         //println!("      {:?}", item_discount);
         //println!("      {:?}", item_old_price);
         //println!("      {:?}", item_discount_price);
-        println!("      {:?}", item_available);
+        //println!("      {:?}", item_available);
         //break;
 
         let item = Item {
@@ -85,9 +112,12 @@ fn main() {
             name: item_name,
             url: item_url,
             discount: item_discount,
-            price: Price,
-            available: bool,
+            price_old: item_old_price,
+            price_discounted: item_discount_price,
+            available: item_available,
         };
+
+        print!("{:#?}", item);
 
     }
 }
