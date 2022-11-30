aaec574654b8333c1b2685d53692e6cd2871cfc9
Sasa Buklijas <buklijas.sasa@gmail.com>
2022-11-30 20:38:38 +0100

    get all items

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use reqwest;
 use scraper::{Html, Selector};
+use std::process;
+use std::thread;
+use std::time::Duration;
 
 // TODO, maybe add price as separate structure
 /*struct Price {
@@ -8,38 +11,83 @@ use scraper::{Html, Selector};
 }*/
 
 #[derive(Debug)]
-//struct Item<'a> {   // <'a> is lifetime
-struct Item {   // <'a> is lifetime
-    //category: &'a str,
-    category: String,   
-    //name: &'a str,
+struct Item {   
+    category: String,
     name: String,
-    //url: &'a str,
     url: String,
     discount: u8,
-    //discount: &'a str,
     price_old: f32,
     price_discounted: f32,
-    //price_old: &'a str,
-    //price_discounted: &'a str,
     available: bool,
 }
 
 struct TzhParser {
-    
+
 }
 
 impl TzhParser {
     const URL: &'static str = "https://www.tvornicazdravehrane.com/proizvodi/popusti-i-akcije/?discount__gt=0&sort=new";
 
-    //fn get_item(&self) -> Item {
-    fn get_items(&self) -> Vec<Item> {
-        let response = reqwest::blocking::get(TzhParser::URL).expect(&format!("Could not load url {}", TzhParser::URL));
-        let body = response.text().unwrap();
+    fn number_of_items_on_page(document: &Html) -> usize {
 
-        let document = Html::parse_document(&body);
         let item_selector = Selector::parse("#items_catalog > div.cp.cp-grid").unwrap();
+        document.select(&item_selector).count()
+    }
+
+    fn get_document_with_all_items(&self)-> Html {
+        // idea was to use CSS selector for load more button, but it is allways display: none;
+            // only when you scrole it is changed("", then display: inline-flex), this aproach is not possible
+        // let item_selector = Selector::parse("a[style=\"display: none;\"]").unwrap(); 
+        // I will use other aproach, counting number of items on page "number_of_items_on_page"
+        // there are few strategies for it
+            // option_1: is to start from 1, and increment by one until you found it
+        
+        let mut page_number:u8 = 1;
+        let mut found = false;
+        loop {
+            let url:&str = &format!("{}&to_page={}", TzhParser::URL, page_number);
+            let response = reqwest::blocking::get(url).expect(&format!("Could not load url {}", url));
+            thread::sleep(Duration::from_millis(2000));   // without sleep, website usually(90%) block after 2 attempts
+            let body = response.text().unwrap(); //println!("{}", body);
+            let document = Html::parse_document(&body);
+
+            let number_of_items = TzhParser::number_of_items_on_page(&document);
+            // default per page is 16 on 29/11/2022
+            let max_number_of_items_for_page_number: usize = usize::from(page_number) * 16;
+            let min_number_of_items_for_page_number = max_number_of_items_for_page_number - 16;
+
+            if number_of_items == max_number_of_items_for_page_number {
+                println!("1:::Page {page_number} there is more than {number_of_items} items on sale.");
+            }
+            // between check
+            else if number_of_items > min_number_of_items_for_page_number && number_of_items < max_number_of_items_for_page_number {
+                println!("FOUND: {} {}", page_number, number_of_items);
+                found = true;
+            }
+            else if number_of_items == 16 {
+                println!("2:::NOT_IMPLEMENTED");
+            }
+            else {
+                eprintln!("ERROR number_of_items={number_of_items}, this is UNEXPECTED, CHECK !!!");
+                process::exit(1);
+
+            }
+            
+            if found {
+                return document
+            }
+            else {
+                page_number = page_number + 1;
+            }
+        }
+
+    }
+
+    fn get_items(&self) -> Vec<Item> {
+        let document_with_all_items = self.get_document_with_all_items();
+
         // must be #items_catalog > .....
+        let item_selector = Selector::parse("#items_catalog > div.cp.cp-grid").unwrap();
         let item_category_selector = Selector::parse("div.cp-cnt a").unwrap();
         let item_name_selector = Selector::parse("div div.cp-title a").unwrap();
         let item_discount_selector = Selector::parse("div div div.cp-badges span").unwrap();
@@ -48,15 +96,13 @@ impl TzhParser {
         let item_discount_price_selector = Selector::parse("div.cp-discount-price").unwrap();
         let item_unavailable_selector = Selector::parse("span.cp-unavailable-label").unwrap();
 
-        let mut vec = Vec::new();
-        for element in document.select(&item_selector) {
+        let mut items = Vec::new();
+        for element in document_with_all_items.select(&item_selector) {
             let item_category_element = element.select(&item_category_selector).next().expect("Could not select item category.");
             let item_category = item_category_element.text().collect::<String>();
-            //let item_category = item_category.as_str();
     
             let item_name_element = element.select(&item_name_selector).next().expect("Could not select item name.");
             let item_name = item_name_element.text().collect::<String>();
-            //let item_name = item_name.as_str();
             let item_url = item_name_element.value().attr("href").expect("Could not find item href attribute.");
             let item_url = item_url.to_string();
         
@@ -71,7 +117,6 @@ impl TzhParser {
             let mut item_old_price = item_old_price_element.text().collect::<String>();
             item_old_price.pop(); 
             item_old_price.pop(); // to remove space 
-            //let item_old_price = item_old_price.trim();
             item_old_price = item_old_price.replace(",", ".");
             let item_old_price = item_old_price.parse::<f32>().unwrap();
             //println!("      {:?}", item_old_price); panic!();
@@ -82,23 +127,14 @@ impl TzhParser {
             let item_discount_price = item_discount_price.trim();
             let item_discount_price = &item_discount_price[0..item_discount_price.len() - 4]; // because it take 2x per char // https://stackoverflow.com/a/65976485/2006674
             //println!("      {:?}", item_discount_price); panic!();
-            //item_discount_price.pop(); 
-            //item_discount_price.pop(); // to remove space
             let item_discount_price = item_discount_price.replace(",", ".");
             let item_discount_price = item_discount_price.parse::<f32>().unwrap();
     
-            let item_unavailable_element = element.select(&item_unavailable_selector).next();        
-            // let item_available = if item_unavailable_element == None {true} else {false};
-            // like this syntaks
+            let item_unavailable_element = element.select(&item_unavailable_selector).next();
             let item_available = match item_unavailable_element {
                 Some(_) => false,
                 None => true,
             };
-            /* 
-            let mut item_available = true;
-            if let Some(_) = item_unavailable_element {
-                item_available = false;
-            }*/
     
             let item = Item {
                 category: item_category,
@@ -109,120 +145,22 @@ impl TzhParser {
                 price_discounted: item_discount_price,
                 available: item_available,
             };
-            vec.push(item);
+            items.push(item);
             //print!("{:#?}", item);
         }
 
-        let vec = vec;
-        vec
+        let items = items;
+        items
     }
 }
 
 
-
 fn main() {
     let p = TzhParser{};
-    let items = p.get_items();
 
-    println!("{:#?}", items);
+    let items = p.get_items();
+    //println!("{:#?}", items);
     println!("Items on sale: {:#?}", items.len());
 
 
 }
-
-// fn main2() {
-//     let url = "https://www.tvornicazdravehrane.com/proizvodi/popusti-i-akcije/?discount__gt=0&sort=new";
-//     //let url = "https://www.tvornicazdravehrane.com/proizvodi/popusti-i-akcije/?discount__gt=0&sort=new&to_page=7";
-//     // https://www.tvornicazdravehrane.com/proizvodi/popusti-i-akcije/?discount__gt=0&sort=new&to_page=6
-//         // da li mogu samo povecati broj stranice i ako ne postoji i da ce mi on vratiti sve elemente
-//             // ne radi tako, ako povecam broj vise nego sto ima, onda vrati samo 1 stranicu 
-//                 // moglo bi se pogadati ++ -- koji je zadni broj, ali to je kompliciranii algoritam, moza poslje implementirati
-//     let response = reqwest::blocking::get(url).expect(&format!("Could not load url {}", url));
-//     let body = response.text().unwrap();
-//     //print!("{}",body); panic!();
-
-//     let document = Html::parse_document(&body);
-//     let item_selector = Selector::parse("#items_catalog > div.cp.cp-grid").unwrap();
-//     // must be #items_catalog > .....
-//     let item_category_selector = Selector::parse("div.cp-cnt a").unwrap();
-//     let item_name_selector = Selector::parse("div div.cp-title a").unwrap();
-//     let item_discount_selector = Selector::parse("div div div.cp-badges span").unwrap();
-//     // price only in Euro, HRK calculated in JS
-//     let item_old_price_selector = Selector::parse("div.cp-old-price > span").unwrap();
-//     let item_discount_price_selector = Selector::parse("div.cp-discount-price").unwrap();
-//     let item_unavailable_selector = Selector::parse("span.cp-unavailable-label").unwrap();  
-
-//     for element in document.select(&item_selector) {
-//         let item_category_element = element.select(&item_category_selector).next().expect("Could not select item category.");
-//         let item_category = item_category_element.text().collect::<String>();
-//         let item_category = item_category.as_str();
-
-//         let item_name_element = element.select(&item_name_selector).next().expect("Could not select item name.");
-//         let item_name = item_name_element.text().collect::<String>();
-//         let item_name = item_name.as_str();
-//         let item_url = item_name_element.value().attr("href").expect("Could not find item href attribute.");
-    
-
-//         let item_discount_element =  element.select(&item_discount_selector).next().expect("Could not select item discount.");
-//         let mut item_discount = item_discount_element.text().collect::<String>();
-//         item_discount.pop();            // remove last char
-//         item_discount.remove(0);    // remove first char    // https://stackoverflow.com/a/70598494/2006674
-//         let item_discount = item_discount.parse::<u8>().unwrap();
-//         //println!("      {:?}", item_discount); panic!();
-
-//         let item_old_price_element =  element.select(&item_old_price_selector).next().expect("Could not select item old price.");
-//         let mut item_old_price = item_old_price_element.text().collect::<String>();
-//         item_old_price.pop(); 
-//         item_old_price.pop(); // to remove space 
-//         //let item_old_price = item_old_price.trim();
-//         item_old_price = item_old_price.replace(",", ".");
-//         let item_old_price = item_old_price.parse::<f32>().unwrap();
-//         //println!("      {:?}", item_old_price); panic!();
-
-//         let item_discount_price_element =  element.select(&item_discount_price_selector).next().expect("Could not select item discount price.");
-//         let item_discount_price = item_discount_price_element.text().collect::<String>();
-//         //println!("      {:?}", item_discount_price);    // "\n\t\t\t\t\t\t\t1,99 €\t\t\t\t\t\t\t\t\t\t\t\t\t"
-//         let item_discount_price = item_discount_price.trim();
-//         let item_discount_price = &item_discount_price[0..item_discount_price.len() - 4]; // because it take 2x per char // https://stackoverflow.com/a/65976485/2006674
-//         //println!("      {:?}", item_discount_price); panic!();
-//         //item_discount_price.pop(); 
-//         //item_discount_price.pop(); // to remove space
-//         let item_discount_price = item_discount_price.replace(",", ".");
-//         let item_discount_price = item_discount_price.parse::<f32>().unwrap();
-
-//         let item_unavailable_element = element.select(&item_unavailable_selector).next();        
-//         // let item_available = if item_unavailable_element == None {true} else {false};
-//         // like this syntaks
-//         let item_available = match item_unavailable_element {
-//             Some(_) => false,
-//             None => true,
-//         };
-//         /* 
-//         let mut item_available = true;
-//         if let Some(_) = item_unavailable_element {
-//             item_available = false;
-//         }*/
-
-//         //println!("{:?}",item_name);
-//         //println!("      {:?}", item_category);
-//         //println!("      {:?}", item_url);
-//         //println!("      {:?}", item_discount);
-//         //println!("      {:?}", item_old_price);
-//         //println!("      {:?}", item_discount_price);
-//         //println!("      {:?}", item_available);
-//         //break;
-
-//         let item = Item {
-//             category: item_category,
-//             name: item_name,
-//             url: item_url,
-//             discount: item_discount,
-//             price_old: item_old_price,
-//             price_discounted: item_discount_price,
-//             available: item_available,
-//         };
-
-//         print!("{:#?}", item);
-
-//     }
-// }
